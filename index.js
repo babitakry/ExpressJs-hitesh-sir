96159164f47f70712209d05494389668c20becc4
Babita Kumari <babitakumari.ky3007@gmail.com>
2025-07-31 21:04:22 +0530

    create express project and add some files .env, .gitignore, src, index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+// Load .env variables
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
