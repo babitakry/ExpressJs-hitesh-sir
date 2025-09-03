d2246cfb2e14087d70af2b1ee1a9f9d838c4b7c4
Babita Kumari <babitakumari.ky3007@gmail.com>
2025-09-04 00:23:21 +0530

    add get login adn get youtube

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
+import 'dotenv/config';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -9,6 +10,14 @@ app.get('/', (req, res) => {
 
 app.get('/twitter', (req, res) => {
   res.send('hiteshdotcom');
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h1> Please login at chai aur code </h1>')
+})
+
+app.get('/youtube', (req, res) => {
+  res.send("<h2>Chai aur code</h2>")
 })
 
 app.listen(port, () => {
